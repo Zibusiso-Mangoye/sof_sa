48f646ede315e71515f97c61ea2362bd78cb6cde
Zibusiso <znmangoye@gmail.com>
2022-01-30 22:36:33 +0200

    Exploration

--- sof_sa/utility_functions.py
+++ sof_sa/utility_functions.py
@@ -15,11 +15,24 @@ def connect2db(**db_credentials):
                             )
     return engine
 
-def db_config(filepath, STAGINGORTARGET):
+def db_config(filepath : str, STAGINGORTARGET : int) -> dict:
+    """
+    Loads database credentials information from a yaml file.
+    Params : 
+        filepath - path to the yaml file
+        STAGINGORTARGET - integer value that chooses the database to use. 
+                        - valid choices are 1 for target db or 0 for staging db
+    Returns : 
+        A nested dictionary containing name of db and credentials
+    """
+    # Checking validity of argument STAGINGORTARGET
+    valid = {0, 1}
+    if STAGINGORTARGET not in valid:
+        raise ValueError("results: status must be one of %r." % valid)
 
-    with open(filepath, "r") as stream:
+    with open(filepath, "r") as f:
         try:
-            parameters = yaml.safe_load(stream)['DATABASE'][STAGINGORTARGET]
+            parameters = yaml.safe_load(f)['DATABASE'][STAGINGORTARGET]
         except yaml.YAMLError as exc:
             print(exc) # log this
     return parameters
