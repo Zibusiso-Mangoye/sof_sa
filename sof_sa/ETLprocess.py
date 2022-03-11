468017cbf8504a465a2f8b562d1d9f62d14203bc
Zibusiso <znmangoye@gmail.com>
2022-03-11 21:53:43 +0200

    Added ETLprocess clean version

--- sof_sa/ETLprocess.py
+++ sof_sa/ETLprocess.py
@@ -1,32 +1,17 @@
 import logging
 import pandas as pd
-from utility_functions import get_credentials, open_db, scrape_data
+from utility_functions import get_credentials, load_data, scrape_data
 
-# Loading data from local csv files
-def extract_raw_data() -> None:
-    
-    logging.info(">> EXTRACTION PROCESS COMPLETED SUCCESSFULLY")
-    
-    # The url that contains salary data 
-    url = 'https://www.globalbrainforce.com/blog/software-developer-salary-around-the-world/'
-    
-    # Data
-    sofraw_df20 = pd.read_csv("..\\data\\survey_results_public_2020.csv", low_memory=False)
-    sofraw_df21 = pd.read_csv("..\\data\\survey_results_public_2021.csv", low_memory=False)
-    web_data = scrape_data(url)
-    
-    try:
-        creds = get_credentials(".\conf\staging_db_credentials.json")
-        engine = open_db(creds)
-    except Exception as err:
-        logging.exception(err, exc_info=1) 
-        raise Exception('>> FAILED TO LOAD IN DATABASE CREDENTIALS')
+# Getting the data from different sources
+url = 'https://www.globalbrainforce.com/blog/software-developer-salary-around-the-world/'
+web_data = scrape_data(url)[3]
+sofraw_df20 = pd.read_csv("..\\data\\survey_results_public_2020.csv", low_memory=False)
+sofraw_df21 = pd.read_csv("..\\data\\survey_results_public_2021.csv", low_memory=False)
 
-    with engine.connect() as connection:
-        sofraw_df20.to_sql('sofraw_20', con=connection, if_exists='replace')
-        sofraw_df21.to_sql('sofraw_21', con=connection, if_exists='replace')
-        web_data.to_sql('pl_salary_staging', con=connection, if_exists='replace')
+# Loading the data to a staging database
+credentials = get_credentials(".\conf\staging_db_credentials.json")
+load_data(web_data, "pl_salary_staging", credentials)
+load_data(sofraw_df20, "stackoverflow2020", credentials)
+load_data(sofraw_df21, "stackoverflow2021", credentials)
 
-    logging.info(">> EXTRACTION PROCESS COMPLETED SUCCESSFULLY")
-
-extract_raw_data
+# Transforming the data
--- sof_sa/utility_functions.py
+++ sof_sa/utility_functions.py
@@ -1,25 +1,24 @@
 import json
 import logging
 import requests
-from sqlalchemy import create_engine
 import pandas as pd
 from contextlib import contextmanager
-
+from sqlalchemy import create_engine, engine
 
 @contextmanager
-def open_db(credentials : dict):
+def open_db(credentials : dict) -> engine.Connection:
     """Connects to the databse specified in the credentials
 
     Args:
-        credentials (dict): Database credentials {"user":"user","password": "password","host": "host","database": "database name"}
+        credentials (dict): Database credentials in the form user, password, host, database name
 
-    Yields:
-        Iterator[mysql.connector.cursor]: a cursor object
+    Returns:
+        engine: a sqlalchemy engine object
     """
     
     try:
         engine = create_engine(f'mysql://{credentials["user"]}:{credentials["password"]}@{credentials["host"]}/{credentials["database"]}')
-        return engine
+        return engine.connect()
     except Exception as error:
         logging.error(error)    
         
@@ -52,10 +51,27 @@ def scrape_data(url):
     try:
         
         r = requests.get(url, headers=header)
-        # Only need the third table from the scraped data
         web_data = pd.read_html(r.text)
         return web_data
     
     except Exception as err:
         logging.exception(err) 
-        raise Exception(f'>> FAILED TO SCRAPE THE URL : {url}')
+        raise Exception(f'>> FAILED TO SCRAPE DATA FROM URL : {url}')
+    
+    
+def load_data(data_to_load : pd.DataFrame, name_of_table : str, credentials : dict) -> None:
+    """Loads data to a database. The database in use is specified in the credentials argument.
+    
+    Args: 
+        - data_to_load(pd.DataFrame): The data to be loaded into the database.
+        - name_of_table(str): The name of the table to be used when load the data to the database.
+        - credentials(dict): A dictionary containing the credentials in the order user, password, host, database name
+        
+    Returns:
+        None 
+    """
+    
+    with open_db(credentials) as connection:
+        data_to_load.to_sql(name_of_table, con=connection, if_exists='replace')
+        
+    logging.info(f"LOADED TABLE WITH NAME: {name_of_table}")
