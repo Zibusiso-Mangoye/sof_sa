052d70ad5b75c4026ccf507ae28214d1742960f2
Zibusiso <znmangoye@gmail.com>
2022-03-11 16:10:21 +0200

    Added extract

--- sof_sa/extract.py
+++ sof_sa/extract.py
@@ -1,46 +1,25 @@
-from sqlalchemy import false
-from logger import logger
+import logging
 import pandas as pd
-from utility_functions import connect2db, db_config, scrape_data
+from utility_functions import get_credentials, open_db, scrape_data
 
-# Setting up logging 
-#File to log to
-log_file = '.\\logs\\extraction.log'
-log = logger(log_file)
-
-log.info(">> EXTRACTION PROCESS STARTED")
-
-def scrape_data(url):
-    """
-    A function to scrape data from the web.
-    Params :
-        url - url for the web page to be scraped.
-    Returns:
-        Tables in the web page in a list of pandas dataframe objects
-    """
-    header = {
-    "User-Agent" : "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.75 Safari/53>",
-    "X-Requested-With" : "XMLHttpRequest"
-    }
-    try:
-        r = requests.get(url, headers=header)
-        # Only need the third table from the scraped data
-        web_data = pd.read_html(r.text)[3]
-        
-        return web_data
-    except Exception as err:
-        log.exception(err) 
-        raise Exception(f'>> FAILED TO SCRAPE THE URL : {url}')
-    
 # Loading data from local csv files
-def load_raw_data_to_db(web_data, sofraw_df20, sofraw_df21):
+def extract_raw_data() -> None:
     
-    log.info(">> LOADING DATABASE CREDENTIALS")
+    logging.info(">> EXTRACTION PROCESS COMPLETED SUCCESSFULLY")
+    
+    # The url that contains salary data 
+    url = 'https://www.globalbrainforce.com/blog/software-developer-salary-around-the-world/'
+    
+    # Data
+    sofraw_df20 = pd.read_csv("..\\data\\survey_results_public_2020.csv", low_memory=False)
+    sofraw_df21 = pd.read_csv("..\\data\\survey_results_public_2021.csv", low_memory=False)
+    web_data = scrape_data(url)
+    
     try:
-        creds = db_config("..\\dev_dbconfig.yaml", 0)
-        engine = connect2db(**creds)
+        creds = get_credentials(".\conf\staging_db_credentials.json")
+        engine = open_db(creds)
     except Exception as err:
-        log.exception(err, exc_info=1) 
+        logging.exception(err, exc_info=1) 
         raise Exception('>> FAILED TO LOAD IN DATABASE CREDENTIALS')
 
     with engine.connect() as connection:
@@ -48,4 +27,6 @@ def load_raw_data_to_db(web_data, sofraw_df20, sofraw_df21):
         sofraw_df21.to_sql('sofraw_21', con=connection, if_exists='replace')
         web_data.to_sql('pl_salary_staging', con=connection, if_exists='replace')
 
-    log.info(">> EXTRACTION PROCESS COMPLETED SUCCESSFULLY")
+    logging.info(">> EXTRACTION PROCESS COMPLETED SUCCESSFULLY")
+
+extract_raw_data
--- sof_sa/utility_functions.py
+++ sof_sa/utility_functions.py
@@ -1,42 +1,61 @@
-import yaml
+import json
+import logging
 import requests
-import pandas as pd
 from sqlalchemy import create_engine
+import pandas as pd
+from contextlib import contextmanager
 
-def connect2db(**db_credentials):
-    """
-    Handles database connections using sqlalchemy's create_engine function
-    Params :
-        db_credentials - a dictionary that contains user, password, host, port and database name 
-    Returns :
-        an sqlalchemy engine object
-    """
 
-    engine = create_engine(f"mysql+pymysql://{db_credentials['DBUSER']}:{db_credentials['DBPASSWORD']}@{db_credentials['DBHOST']}:{db_credentials['DBPORT']}/{db_credentials['DBNAME']}")
-    return engine
+@contextmanager
+def open_db(credentials : dict):
+    """Connects to the databse specified in the credentials
 
-def db_config(filepath : str, which_db : int) -> dict:
+    Args:
+        credentials (dict): Database credentials {"user":"user","password": "password","host": "host","database": "database name"}
+
+    Yields:
+        Iterator[mysql.connector.cursor]: a cursor object
     """
-    Loads database credentials information from a yaml file.
-    Params : 
-        filepath - path to the yaml file
-        stagingortarget - integer value that chooses the database to use. 
-                        - valid choices are 1 for target db or 0 for staging db
-    Returns : 
-        A nested dictionary containing name of db and credentials
-    """
-    valid = {0, 1}
-    if which_db not in valid:
-        raise ValueError("results: status must be one of %r." % valid)
     
-    db = 'SOF_DB'
-    if which_db == 0:
-        db = 'STAGING_DB'
+    try:
+        engine = create_engine(f'mysql://{credentials["user"]}:{credentials["password"]}@{credentials["host"]}/{credentials["database"]}')
+        return engine
+    except Exception as error:
+        logging.error(error)    
+        
 
-    with open(filepath, "r") as f:
-        try:
-            parameters = yaml.safe_load(f)['DATABASE'][which_db][db]
-            
-        except yaml.YAMLError as exc:
-            print(exc) # log this
-    return parameters
+def get_credentials(filepath : str) -> dict:
+    """Loads database credentials from file.
+    Args: 
+        filepath - path to the json file
+
+    Returns :
+        A dictionary containing database credentials
+    """
+    
+    with open(filepath, "r") as file:
+        data = json.loads(file.read())
+   
+    return data
+
+def scrape_data(url):
+    """A function to scrape data from the web.
+    Args:
+        url - url for the web page to be scraped.
+    Returns:
+        Tables in the web page in a list of pandas dataframe objects
+    """
+    header = {
+    "User-Agent" : "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.75 Safari/53>",
+    "X-Requested-With" : "XMLHttpRequest"
+    }
+    try:
+        
+        r = requests.get(url, headers=header)
+        # Only need the third table from the scraped data
+        web_data = pd.read_html(r.text)
+        return web_data
+    
+    except Exception as err:
+        logging.exception(err) 
+        raise Exception(f'>> FAILED TO SCRAPE THE URL : {url}')
